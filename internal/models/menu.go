905be98ce4d709c8296133656f1ea37707a4530e
ask <2506377990@qq.com>
2025-08-23 22:54:07 +0800

    feat: 添加 ParentID 字段到 MenuModel 结构体，更新菜单模型

--- internal/models/menu.go
+++ internal/models/menu.go
@@ -56,20 +56,23 @@ func (a *ApiIds) UnmarshalJSON(data []byte) error {
 }
 
 type MenuModel struct {
-	ID           uuid.UUID `json:"id" gorm:"primaryKey;type:char(36);comment:唯一ID"`                        // 唯一ID
-	Name         string    `json:"name" gorm:"size:64;not null;comment:菜单名称"`                              // 菜单名称
-	RouteName    string    `json:"route_name" gorm:"size:64;not null;unique;comment:路由名称"`                 // 路由名称，唯一
-	RoutePath    string    `json:"route_path" gorm:"size:255;not null;comment:路由路径"`                       // 路由路径
-	ApiIds       *ApiIds   `json:"api_ids" gorm:"type:text;comment:API路径"`                                 // API路径
-	IsHidden     bool      `json:"is_hidden" gorm:"type:boolean;not null;default:false;comment:是否隐藏"`      // 是否隐藏
-	IsFullScreen bool      `json:"is_full_screen" gorm:"type:boolean;not null;default:false;comment:是否全屏"` // 是否全屏
-	IsTabs       bool      `json:"is_tabs" gorm:"type:boolean;not null;default:false;comment:是否添加到tabs"`   // 是否添加到tabs
-	Component    string    `json:"component" gorm:"size:255;not null;comment:组件路径"`                        // 组件路径
-	Icon         *string   `json:"icon" gorm:"size:64;comment:侧边栏图标"`                                      // 侧边栏图标
-	Order        uint      `json:"order" gorm:"type:int;not null;default:0;comment:排序"`                    // 排序
+	ID           uuid.UUID  `json:"id" gorm:"primaryKey;type:char(36);comment:唯一ID"`                        // 唯一ID
+	ParentID     *uuid.UUID `json:"parent_id" gorm:"type:char(36);comment:父级ID"`                            // 父级ID
+	Name         string     `json:"name" gorm:"size:64;not null;comment:菜单名称"`                              // 菜单名称
+	RouteName    string     `json:"route_name" gorm:"size:64;not null;unique;comment:路由名称"`                 // 路由名称，唯一
+	RoutePath    string     `json:"route_path" gorm:"size:255;not null;comment:路由路径"`                       // 路由路径
+	ApiIds       *ApiIds    `json:"api_ids" gorm:"type:text;comment:API路径"`                                 // API路径
+	IsHidden     bool       `json:"is_hidden" gorm:"type:boolean;not null;default:false;comment:是否隐藏"`      // 是否隐藏
+	IsFullScreen bool       `json:"is_full_screen" gorm:"type:boolean;not null;default:false;comment:是否全屏"` // 是否全屏
+	IsTabs       bool       `json:"is_tabs" gorm:"type:boolean;not null;default:false;comment:是否添加到tabs"`   // 是否添加到tabs
+	Component    string     `json:"component" gorm:"size:255;not null;comment:组件路径"`                        // 组件路径
+	Icon         *string    `json:"icon" gorm:"size:64;comment:侧边栏图标"`                                      // 侧边栏图标
+	Order        uint       `json:"order" gorm:"type:int;not null;default:0;comment:排序"`                    // 排序
 
 	CommonModel
 }
+
+// TODO:缺少按钮表,需要和菜单表进行关联
 
 // TableName 设置表名
 func (MenuModel) TableName() string {
